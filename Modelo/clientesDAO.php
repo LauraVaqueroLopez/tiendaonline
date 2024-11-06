7a62b49d117d2f5635622b4ba793fdb1bf331e6b
Ymko.bami_ <154588281+LauraVaqueroLopez@users.noreply.github.com>
2024-11-06 12:37:01 +0100

    Update clientesDAO.php

--- Modelo/clientesDAO.php
+++ Modelo/clientesDAO.php
@@ -1,35 +1,33 @@
 <?php
-require_once 'db.php';
+require_once 'DB.php';
 require_once 'DTOclientes.php';
 
-class EmpleadoDAO {
+class ClienteDAO {
     private $conn;
+
     public function __construct() {
         $this->conn = DB::getConnection();
     }
-    
+
     public function getClienteById($id) {
         $stmt = $this->conn->prepare("SELECT * FROM Cliente WHERE id = :id");
         $stmt->bindParam(':id', $id);
         $stmt->execute();
         $fila = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $fila ? new DTOclientes($fila['id'], $fila['nombre']) : null;
+    }
 
-        if ($fila) {
-            return new DTOCliente($fila['id'], $fila['nombre']);
-        } else {
-            return null; // Si no se encuentra, devolvemos null
-        }
+    public function getClienteByNombre($nombre) {
+        $stmt = $this->conn->prepare("SELECT * FROM Cliente WHERE nombre = :nombre");
+        $stmt->bindParam(':nombre', $nombre);
+        $stmt->execute();
+        $fila = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $fila ? new DTOclientes($fila['id'], $fila['nombre']) : null;
     }
 
     public function addCliente($cliente) {
         $stmt = $this->conn->prepare("INSERT INTO Cliente (nombre) VALUES (:nombre)");
         $stmt->bindParam(':nombre', $cliente->getNombre());
-        return $stmt->execute();
-    }
-
-    public function deleteCliente($id) {
-        $stmt = $this->conn->prepare("DELETE FROM Cliente WHERE id = :id");
-        $stmt->bindParam(':id', $id);
         return $stmt->execute();
     }
 }
