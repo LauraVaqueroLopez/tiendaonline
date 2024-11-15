4e24a75371fcdd8d7f68cf1f01a90c489016a9cf
Papaseit <161719291+Papaseit@users.noreply.github.com>
2024-11-15 09:45:39 +0100

    Add files via upload

---
+++ Controlador/ControlProducto.php
@@ -0,0 +1,21 @@
+<?php
+require_once '../Modelo/ProductoDAO.php';
+
+class ControlProducto
+{
+    public function listarProductos()
+    {
+        $productoDAO = new ProductoDAO();
+        return $productoDAO->getAllProductos();
+    }
+
+    public function obtenerProductoPorId($id_producto) {
+        $productoDAO = new ProductoDAO();
+        return $productoDAO->carritoProductoPorId($id_producto);
+    }
+}
+
+
+
+
+?>
